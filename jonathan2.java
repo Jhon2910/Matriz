200a85d049683857334f011758796dbeddcbac44
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-08-07 08:43:23 -0300

    Update jonathan2.java

--- jonathan2.java
+++ jonathan2.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
 public class jonathan2 {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        
 
         int linha = 3;
         int coluna = 5;
