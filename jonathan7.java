e6d41571cb4ab9f8be25b61911e7f6bc2a4d6c30
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-08-20 15:06:09 -0300

    Update jonathan7.java

--- jonathan7.java
+++ jonathan7.java
@@ -1,41 +1,50 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
-public class jonathan7 {
+public class jonathan9 {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         Random r = new Random();
+        int linhas;
+        int col;
 
-        int n;
+        System.out.println("Digiter as linhas da matriz: ");
+        while (!sc.hasNextInt()) {
+            System.out.println("Digite apenas numeros inteiros.");
+            System.out.println("Digiter as linhas da matriz: ");
+            sc.next();
+        }
+        linhas = sc.nextInt();
 
-        System.out.println("Digite a dimensao: ");
-        n = sc.nextInt();
+        System.out.println("Digite as colunas da matriz: ");
+        while (!sc.hasNextInt()) {
+            System.out.println("Digite apenas numeros inteiros.");
+            System.out.println("Digiter as colunas da matriz: ");
+            sc.next();
+        }
+        col = sc.nextInt();
 
-        int matriz[][] = new int[n][n];
-        int linha[] = new int[n];
-        int coluna[] = new int[n];
+        int matriz[][] = new int[linhas][col];
+        int somaLinha[] = new int[matriz.length];
+        int somaColuna[] = new int[matriz.length];
 
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                System.out.println("Digite o numero: ");
-                matriz[i][j] = sc.nextInt();
+                matriz[i][j] = r.nextInt(0, 2);
+                somaColuna[i] += matriz[i][j];
+                somaLinha[i] += matriz[i][j];
             }
         }
 
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                linha[i] += matriz[i][j];
-                coluna[j] += matriz[i][j];
-            }
-        }
-
-        for (int i = 0; i < matriz.length;i++){
-            if (linha[i] != 1 || coluna[i] != 1){
-                System.out.println("nao e uma matriz permutacao.");
+            if (somaColuna[i] > 1 || somaLinha[i] > 1) {
+                System.out.println("N");
                 return;
             }
         }
 
-        System.out.println("e uma matriz permutação.");
+        System.out.println("S");
+
     }
 }
